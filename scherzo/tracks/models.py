72819e82efa1779a0d902e714cabad59709a570f
asimonia <alex.simonian@gmail.com>
2017-03-19 18:34:39 -0500

    Add abstract base model and content types

--- scherzo/tracks/models.py
+++ scherzo/tracks/models.py
@@ -67,6 +67,43 @@ class Content(models.Model):
 					the two previous fields
 	"""
 	module = models.ForeignKey(Module, related_name='contents')
-	content_type = models.ForeignKey(ContentType)
+	content_type = models.ForeignKey(ContentType, limit_choices_to={'model__in':('text',
+																	'video',
+																	'image',
+																	'file')})
 	object_id = models.PositiveIntegerField()
 	item = GenericForiegnKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+	"""
+	Abstract model that will serve as parent to the child content types
+	that will inherit the base characteristics.
+	Text, File, Image, and Video
+	"""
+	owner = models.ForeignKey(User, related_name='%(class)s_related')
+	title = models.CharField(max_length=250)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
+
+	def __str__(self):
+		return self.title
+
+
+class Text(ItemBase):
+	content = models.TextField()
+
+
+class File(ItemBase):
+	file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+	file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+	url = models.URLField()
