90c92bbc26f2d6e5d1de959af3dd6efb3c79e1a4
asimonia <alex.simonian@gmail.com>
2017-03-19 16:20:12 -0500

    Register models to admin panel

--- scherzo/tracks/admin.py
+++ scherzo/tracks/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Track, Course, Module
 
-# Register your models here.
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+	list_display = ['title', 'slug']
+	prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+	model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+	list_display = ['title', 'track', 'created']
+	list_filter = ['created', 'track']
+	prepopulated_fields = {'slug': ('title',)}
+	search_filter = ['title', 'overview']
+	inlines = [ModuleInline]
+
+
