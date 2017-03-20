f525ba44b9ef612914d569d5247803a2e32b177e
asimonia <alex.simonian@gmail.com>
2017-03-20 18:09:00 -0500

    Create CRUD views using CBV mixins for course

--- scherzo/tracks/views.py
+++ scherzo/tracks/views.py
@@ -1,15 +1,49 @@
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Course
 
 
-
-class ManageCourseListView(ListView):
-	model = Course
-	template_name = 'courses/manage/course/list.html'
+class OwnerMixin:
 
 	def get_queryset(self):
 		qs = super().get_queryset()
 		return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+
+	def form_valid(self, form):
+		form.instance.owner = self.request.user
+		return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+	model = Course
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+	fields = ['track', 'title', 'slug', 'overview']
+	success_url = reverse_lazy('manage_course_list')
+	template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+	template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+	pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+	pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+	template_name = 'courses/manage/course/delete.html'
+	success_url = reverse_lazy('manage_course_list')
 
 
 
