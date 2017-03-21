926d94b45bedd1fe2dd71869ae0ef37775f39896
asimonia <alex.simonian@gmail.com>
2017-03-20 19:57:21 -0500

    Add permissions and login required mixins

--- scherzo/tracks/views.py
+++ scherzo/tracks/views.py
@@ -1,3 +1,5 @@
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -19,8 +21,10 @@ class OwnerEditMixin:
 		return super().form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
 	model = Course
+	fields = ['track', 'title', 'slug', 'overview']
+	success_url = reverse_lazy('manage_course_list')
 
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
@@ -33,17 +37,20 @@ class ManageCourseListView(OwnerCourseMixin, ListView):
 	template_name = 'courses/manage/course/list.html'
 
 
-class CourseCreateView(OwnerCourseEditMixin, CreateView):
-	pass
+class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
+	permission_required = 'courses.add_course'
 
 
-class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-	pass
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin, UpdateView):
+	template_name = 'courses/manage/course/form.html'
+	permission_required = 'courses.change_course'
 
 
-class CourseDeleteView(OwnerCourseMixin, DeleteView):
+class CourseDeleteView(PermissionRequiredMixin, OwnerCourseMixin, DeleteView):
 	template_name = 'courses/manage/course/delete.html'
 	success_url = reverse_lazy('manage_course_list')
+	permission_required = 'courses.delete_course'
+	
 
 
 
