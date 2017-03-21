49a522abfc02b4ac785d52aef66375d890d059b3
asimonia <alex.simonian@gmail.com>
2017-03-20 20:01:53 -0500

    Add url patterns

--- scherzo/scherzo/urls.py
+++ scherzo/scherzo/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^course/', include('courses.urls')),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
 ]
