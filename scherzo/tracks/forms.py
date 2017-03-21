793d5d99c82b6c26059bfb4875624a7a56154022
asimonia <alex.simonian@gmail.com>
2017-03-20 22:52:24 -0500

    Fix model

--- scherzo/tracks/forms.py
+++ scherzo/tracks/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.forms.models import inlineformset_factory
-from .models import Track, Module
+from .models import Course, Module
 
 
-ModuleFormSet = inlineformset_factory(Track, Module, 
-	fields=['title', 'description'], extra=2, can_delete=True)
+ModuleFormSet = inlineformset_factory(Course, Module, fields=['title', 'description'], extra=2, can_delete=True)
