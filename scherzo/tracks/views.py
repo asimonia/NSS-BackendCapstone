88fed73e9994c6c73a8f612f9faee459c3dc5b37
asimonia <alex.simonian@gmail.com>
2017-03-21 15:43:39 -0500

    Add module and content order views for DnD interface

--- scherzo/tracks/views.py
+++ scherzo/tracks/views.py
@@ -1,4 +1,5 @@
 from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.list import ListView
@@ -153,3 +154,21 @@ class ModuleContentListView(TemplateResponseMixin, View):
 	def get(self, request, module_id):
 		module = get_object_or_404(Module, id=module_id, course__owner=request.user)
 		return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+	"""Reorder modules based on the DnD interface"""
+	def post(self, request):
+		for id, order in self.request_json.items():
+			Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+		return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+	"""Reorder contents based on the DnD interface"""
+	def post(self, request):
+		for id, order in self.request_json.items():
+			Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+		return self.render_json_response({'saved': 'OK'})
+
+
