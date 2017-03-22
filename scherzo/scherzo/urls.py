33a175c765c9141f948eef297fde733a71dc14ee
asimonia <alex.simonian@gmail.com>
2017-03-22 13:33:00 -0500

    Add to urls

--- scherzo/scherzo/urls.py
+++ scherzo/scherzo/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from tracks.views import CourseListView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^course/', include('tracks.urls')),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
+    url(r'^$', CourseListView.as_view(), name='course_list'),
 ]
