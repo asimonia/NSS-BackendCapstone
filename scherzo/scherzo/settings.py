521899b158b68ba783042482b1414d06b1b44287
asimonia <alex.simonian@gmail.com>
2017-03-22 19:36:31 -0500

    Add MEDIA URLs to settings

--- scherzo/scherzo/settings.py
+++ scherzo/scherzo/settings.py
@@ -89,5 +89,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
 LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
